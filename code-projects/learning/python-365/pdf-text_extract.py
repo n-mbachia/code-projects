e65aa2cd2d3f2e8780f8bcc4c4e88df098205cf0
n-mbachia <ndmbachia@outlook.com>
2023-03-15 00:04:56 +0300

    pdf text extract

---
+++ code-projects/learning/python-365/pdf-text_extract.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This code was adopted from Python Coding YouTube Channel Day 25"""
+
+import PyPDF2
+
+filename = input("Enter the file name of a PDF file: ")
+search_term = input("Enter the seatch term: ")
+
+with open(filename, "rb") as file:
+    reader = PyPDF2.PdfReader(file)
+    num_pages = len(reader.pages)
+
+    for page in range(num_pages):
+        current_page = reader.pages[page]
+        text = current_page.extract_text()
+        if search_term in text:
+            print(f"Page {page+1}:")
+           # print(text)
