a14df4e68faf6c33543b84f9bcf9df1e99358ae9
n-mbachia <ndmbachia@outlook.com>
2023-04-02 22:57:50 +0300

    font art

---
+++ code-projects/learning/python-365/font_art.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""This code was adopted from Python Coding YouTube channel day 35..."""
+
+import pyfiglet # pip3 install pyfiglet
+
+font = pyfiglet.figlet_format(input("Enter any word: "))
+
+print(font)
+
