d623d64da4012752542636ba4b7e64bf79a3041c
n-mbachia <ndmbachia@outlook.com>
2023-04-11 07:28:33 +0300

    euclidean changes

--- code-projects/learning/30DaysOfPython/day_1/euclidean_distance.py
+++ code-projects/learning/30DaysOfPython/day_1/euclidean_distance.py
@@ -4,5 +4,5 @@ import math
 
 x = (2, 3)
 y = (10, 8)
-distance = math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))
+distance = ((y[0] - x[0])**2 + (y[1] - x[1])**2)**0.5 
 print(f"Eulidean distance from x to y:",distance)
