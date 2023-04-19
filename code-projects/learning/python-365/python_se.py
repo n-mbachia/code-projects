d51272713d56ba098f52204a8819f43d8c30b4f4
n-mbachia <ndmbachia@outlook.com>
2023-04-19 21:43:37 +0300

    search engine

---
+++ code-projects/learning/python-365/python_se.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""This code was adopted from Python Coding YouTube Channel day 43..."""
+
+import pywhatkit as kit
+searchitem = (input("Enter the topic: "))
+
+kit.info(searchitem,100)
