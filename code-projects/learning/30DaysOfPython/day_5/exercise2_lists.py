9248c02276c5ea08caac2987fc0437ee2bb6d4b0
n-mbachia <ndmbachia@outlook.com>
2023-04-21 23:37:26 +0300

    lists

---
+++ code-projects/learning/30DaysOfPython/day_5/exercise2_lists.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+ages_sorted = age.sorted()
+
+# find min and max age
+min_age = min(ages_sorted)
+max_age = max(ages_sorted)
+
+# add min and max ages to the list
+ages_updated = ages_sorted + [min_age, max_age]
+
+# find the median age
+n = len(ages_updated)
+n % 2 == 0
+
+print(age)
+print(age.min())
