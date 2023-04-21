644d319f9b36ab5381c98852d81135416d06fc68
n-mbachia <ndmbachia@outlook.com>
2023-04-21 22:52:16 +0300

    lists

---
+++ code-projects/learning/30DaysOfPython/day_5/lists.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+accessories = ['shoes', 'belt', 'wallet', 'pants', 'vest', 'tshirt', 'boxers']
+print(accessories)
+print()
+print(len(accessories))
+print()
+print(accessories[::3])
+print()
+mixed_data_types = ['mbachia', '32', '5\'6\"', 'single', 'kanairo']
+it_companies =['Facebook', 'Google', 'Microsoft', 'Apple', 'Oracles', 'Amazon']
+print(it_companies)
+print(len(it_companies))
+print()
+print(it_companies[::2])
+print()
+it_companies.append('Netflix')
+print(it_companies)
+print()
+it_companies.insert(4, 'Dell')
+print(it_companies)
+print()
+it_companies.isupper('Facebook')
+print(it_companies)
