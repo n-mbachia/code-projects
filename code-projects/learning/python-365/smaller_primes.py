07312a4fa4b2a8fff19763c2b95282504747915a
Mbachia Ng'ethe <ndmbachia@outlook.com>
2023-05-10 21:08:08 +0300

    smallest prime edited

--- code-projects/learning/python-365/smaller_primes.py
+++ code-projects/learning/python-365/smaller_primes.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
 """This code was adopted from Python Coding YouTube channel day 51..."""
 
-def sieve_off(n):
-    primes = [True] * (n +1)
-    p = 2 #smallest prime number is 2
-    while (p * p <= n): 
-        if primes[p] == True: #test true for prime number only
-            for i in range (p *2, n + 1, p): # marks all mutiples of numbers as False
-                primes[i] = False
-        p += 1 
+def sieve_of_eratosthenes(n):
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False  # 0 and 1 are not prime
 
-    for i in range(2, n): #prints all prime numbers
-        if primes[i]:
-            print(i)
+    p = 2
+    while p * p <= n:
+        if primes[p]:
+            for i in range(p * p, n + 1, p):
+                primes[i] = False
+        p += 1
+
+    prime_numbers = [num for num, is_prime in enumerate(primes) if is_prime]
+    return prime_numbers
+
+
 if __name__ == '__main__':
-    n = int(input("Enter a no to check all smaller prime numbers: "))
-    sieve_off(n)
+    n = int(input("Enter a number to check all smaller prime numbers: "))
+    primes = sieve_of_eratosthenes(n)
+    print("Prime numbers smaller than or equal to", n, ":")
+    print(primes)
+
