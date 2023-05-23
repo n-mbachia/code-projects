01fcce3750af8a64cd78ee60a5fa94a643576271
Mbachia Ng'ethe <ndmbachia@outlook.com>
2023-05-23 22:10:15 +0300

    screenshot babe

---
+++ code-projects/learning/python-365/screenshot.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This code was adopted from the python coding YouTube channel day 55..."""
+
+import pyautogui #pip install pyautogui
+
+my_screen_shot = pyautogui.screenshot()
+
+my_screen_shot.save('/users/mbachia/code-projects/learning/python-365/screenshot.png')
+
