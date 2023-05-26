9794db07345a4da341fd7f475cdc9e5a882f9b4a
Mbachia Ng'ethe <ndmbachia@outlook.com>
2023-05-27 00:13:31 +0300

    scatter plot

---
+++ code-projects/learning/python-365/scatter_plot.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This code was adopted from Python Coding YouTube channel day 57..."""
+
+import matplotlib.pyplot as pyplot
+
+# create data
+riding = ((17, 18, 21, 22, 19, 21, 25, 22, 25, 24), (3, 6, 3.5, 4, 5, 6.3, 4.5, 5, 4.5, 4))
+swimming = ((17, 18, 20, 19, 22, 21, 21, 23, 19, 21), (8, 9, 7, 10, 7.5, 9, 8, 7, 8.5, 9))
+sailing = ((31, 28, 29, 36, 27, 32, 34, 35, 33, 39), (4, 6.3, 3, 5, 7.5, 4.6, 2, 7, 4, 3.8))
+
+# plot the data
+pyplot.scatter(x=riding[0], y=riding[1], c='red', marker='s', label='riding')
+pyplot.scatter(x=swimming[0], y=swimming[1], c='green', marker='o', label='swimming')
+pyplot.scatter(x=sailing[0], y=sailing[1], c='blue', marker='^', label='sailing')
+
+# configure graph
+pyplot.xlabel('Age')
+pyplot.ylabel('Hours')
+pyplot.title('Activities scatter graph')
+pyplot.legend()
+pyplot.show()
