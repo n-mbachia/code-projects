9b6662524950478ef767d9d8f37351d42edcd79a
Mbachia Ng'ethe <ndmbachia@outlook.com>
2023-05-29 00:18:06 +0300

    file protection success

--- code-projects/learning/python-365/file_protection.py
+++ code-projects/learning/python-365/file_protection.py
@@ -1,17 +1,17 @@
 #!/usrs/bin/python3
 """This code was adopted from Python Coding Youtube channel day 57..."""
 
-from PyPDF2 import PdfFileWriter, PdfFileReader
+from PyPDF2 import PdfWriter, PdfReader
 import getpass
 
 pdfwriter=PdfWriter()
-pdf = pdfFilereader("Users/mbachia/code-projects/learning/python-365/36 Questions.pdf")
-for page_num in range(pdf.numPage):
-    pdfwriter.addPage(pdf.getPage(page_num))
+pdf = PdfReader("/Users/mbachia/code-projects/learning/python-365/36 Questions.pdf")
+for page_number in range(len(pdf.pages)):
+    pdfwriter.add_page(pdf.pages[page_number])
 password=getpass.getpass(prompt="Enter a password: ")
 pdfwriter.encrypt(password)
-with open("Users/mbachia/code-projects/learning/python-365/36 Questions.pdf") as f:
+with open("/Users/mbachia/code-projects/learning/python-365/36 Questions.pdf", "wb") as f:
     pdfwriter.write(f)
 
-print("Now file is passwrod protected")
+print("Now file is password protected")
 
