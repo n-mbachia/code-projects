79296f5259381daa5330387225ca68a3bf96b871
Mbachia Ng'ethe <ndmbachia@outlook.com>
2023-07-04 22:54:28 +0300

    list swap function

---
+++ code-projects/learning/python-365/swap_function.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This code was adopted from Python coding YouTube channel day 70"""
+
+# Swap function
+def swaplist(newlist):
+    size = len(newlist)
+
+    temp = newlist(0)
+    newlist[0] = newlists[size -1]
+    newlist [size -1] = temp
+
+    return newlist
+
+
+newlist = []
+newlist = [int(item) for item in inpu{"Enter the list items").split()]
+
+print(swapList(newlist)
+
+
+
