d981b6d14479917a7413b9ca6fa93b02a49d7fd0
Mbachia Ng'ethe <ndmbachia@outlook.com>
2023-09-05 23:40:48 +0300

    comments added

--- code-projects/learning/30DaysOfPython/day_10/level_1/hash_square.py
+++ code-projects/learning/30DaysOfPython/day_10/level_1/hash_square.py
@@ -2,7 +2,8 @@
 num_rows = 8
 num_cols = 8
 
-for i in range(num_rows):
-    for j in range(num_cols):
-        print("# ", end="" )
-    print("\r")
+for i in range(num_rows): # loops through rows
+    for j in range(num_cols): # loops through columns
+        print("# ", end="" ) # prints chracter followed by space without a newline
+    print("\r") # moves to next row
+
