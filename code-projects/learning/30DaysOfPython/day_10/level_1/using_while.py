a536eaeda5a2ba09683c6901b4f931ae94e85b20
Mbachia Ng'ethe <ndmbachia@outlook.com>
2023-09-05 22:50:10 +0300

    for loop

--- code-projects/learning/30DaysOfPython/day_10/level_1/using_while.py
+++ code-projects/learning/30DaysOfPython/day_10/level_1/using_while.py
@@ -1,6 +1,14 @@
 #!/usrs/bin/python3
 """This iterates from 10 to 0"""
+print("This was printed using the while loop\n")
+
 i = 10 
 while i >= 0:
     print(i)
     i = i - 1
+print("\nThis was printed using the for loop\n")
+
+nums = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+for i in nums:
+    if i <= 10:
+        print(i)
