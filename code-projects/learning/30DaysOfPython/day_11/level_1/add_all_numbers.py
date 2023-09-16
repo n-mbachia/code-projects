e8e913bfb31cd1207e2c4c19ad5e4ac59fec1b3a
Mbachia Ng'ethe <ndmbachia@outlook.com>
2023-09-16 23:54:31 +0300

    docstring added

--- code-projects/learning/30DaysOfPython/day_11/level_1/add_all_numbers.py
+++ code-projects/learning/30DaysOfPython/day_11/level_1/add_all_numbers.py
@@ -2,11 +2,21 @@
 """Write a function called add_all_nums which takes arbitrary number of arguments and sums all the arguments. Check if all the list items are number types. If not do give a reasonable feedback."""
 
 def add_all_nums(*args):
-    total = 0
-    for i in args:
-        try:
-            total += int(i)
-        except ValueError: 
-            return None
-    return(total)
+   """Sum of all arguments, 
+   
+   Arguments:
+     *args = abitrary number of argumners
+   
+   Returns:
+     returns the sum of arguments, if they are all numbers 
+     otherwise raises a TypeError exception
+   """
+
+   total = 0
+   for i in args:
+      try:
+         total += int(i)
+      except ValueError: 
+         return None
+   return(total)
 print(add_all_nums(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
