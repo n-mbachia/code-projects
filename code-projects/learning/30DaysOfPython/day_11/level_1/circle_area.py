521fe2aedb8d64d2d810f4bfc6e2226f6344c598
Mbachia Ng'ethe <ndmbachia@outlook.com>
2023-09-16 23:00:54 +0300

    area of a circle

---
+++ code-projects/learning/30DaysOfPython/day_11/level_1/circle_area.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Area of a circle is calculated as follows: area = π x r x r. Write a function that calculates area_of_circle."""
+
+def area_of_circle(r):
+    circle_area = (3.142 * r * r)
+    return(circle_area)
+print(area_of_circle(7))
