393477d4128c249c969c313b30a1caf1895c183b
Mbachia Ng'ethe <ndmbachia@outlook.com>
2023-09-16 22:55:11 +0300

    add two numbers

---
+++ code-projects/learning/30DaysOfPython/day_11/level_1/func_sum.py
@@ -0,0 +1,8 @@
+#!/usrs/bin/python3
+"""Declare a function add_two_numbers. It takes two parameters and it returns a sum."""
+def add_two_numbers(a, b):
+    """Funtions takes two pre-defined numbers and returns their sum"""
+    sum_numbers = a + b
+    return(sum_numbers)
+
+print(add_two_numbers(5, 10))
