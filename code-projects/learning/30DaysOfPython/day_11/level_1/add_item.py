cc12f633f8ea263bc2bbe6e02837e4f1f09f1b50
Mbachia Ng'ethe <ndmbachia@outlook.com>
2023-09-17 13:39:14 +0300

    list add item completed

---
+++ code-projects/learning/30DaysOfPython/day_11/level_1/add_item.py
@@ -0,0 +1,11 @@
+#!/usrs/bin/python3
+"""Declare a function named remove_item. It takes a list and an item parameters. It returns a list with the item removed from it."""
+
+def add_item(list1, item):
+    list1.append(item)
+    return list1
+
+food_staff = ['Potato', 'Tomato', 'Mango', 'Milk']
+print(add_item(food_staff, 'Mango')) 
+numbers = [2, 3, 7, 9]
+print(add_item(numbers, 3))
