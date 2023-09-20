626c25f551b8205d502910743c55f3ecbf92c1c1
Mbachia Ng'ethe <ndmbachia@outlook.com>
2023-09-20 21:43:58 +0300

    shuffled list comments

--- code-projects/learning/30DaysOfPython/day_12/level_2/shuffle_list.py
+++ code-projects/learning/30DaysOfPython/day_12/level_2/shuffle_list.py
@@ -13,10 +13,10 @@ def shuffle_list(list1):
       A shuffled list. 
     """
 
-    shuffled_list = list1.copy()
-    for i in range(len(shuffled_list)):
-        rand_index = random.randint(0, len(shuffled_list) -1)
-        shuffled_list[i], shuffled_list[rand_index] = shuffled_list[rand_index], shuffled_list[i]
+    shuffled_list = list1.copy() # Creates a copy of list argument as passed
+    for i in range(len(shuffled_list)): # iterates over elements
+        rand_index = random.randint(0, len(shuffled_list) -1) # generates a random index based on length of copy
+        shuffled_list[i], shuffled_list[rand_index] = shuffled_list[rand_index], shuffled_list[i] # swaps current element with random index
 
 
     return shuffled_list
