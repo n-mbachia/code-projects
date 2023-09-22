a066e0afc3fe887606339cc9ce535e206425929f
Mbachia Ng'ethe <ndmbachia@outlook.com>
2023-09-23 00:39:20 +0300

    working comment

--- code-projects/learning/30DaysOfPython/day_13/task_3.py
+++ code-projects/learning/30DaysOfPython/day_13/task_3.py
@@ -2,3 +2,13 @@
 """Using list comprehension create the following list of tuples:"""
 list_of_tuple = [(i, 1, i**0, i**1, i**2, i**3, i**4) for i in range(11)]
 print(list_of_tuple)
+"""
+This code works by first iterating over the range of numbers from 0 to 10 using the for loop. For each number, the code then creates a tuple containing the number, 1, and the number raised to the powers of 0, 1, 2, 3, and 4. The code then adds the tuple to the list of tuples.
+
+Here is a breakdown of the code:
+
+list_of_tuple = []: This creates an empty list to store the tuples.
+for i in range(11): This iterates over the range of numbers from 0 to 10.
+(i, 1, i**0, i**1, i**2, i**3, i**4): This creates a tuple containing the number, 1, and the number raised to the powers of 0, 1, 2, 3, and 4.
+list_of_tuple.append((i, 1, i**0, i**1, i**2, i**3, i**4)): This adds the tuple to the list of tuples.
+"""
