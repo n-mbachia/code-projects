29b35ed4ca71cd9386d31fe38aa21bb92a1b72b7
Mbachia Ng'ethe <ndmbachia@outlook.com>
2023-09-26 23:09:03 +0300

    task 5 complete

---
+++ code-projects/learning/30DaysOfPython/day_14/level_1/task_5.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Use for to print each name in the names list."""
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+
+def print_names():
+    for name in names:
+        return names
+print(names)
