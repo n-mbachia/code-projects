d02ee1f1b9a950c6b7656b14ca350c0c3bae8fef
Mbachia Ng'ethe <ndmbachia@outlook.com>
2023-09-26 23:12:11 +0300

    task 6 complete

---
+++ code-projects/learning/30DaysOfPython/day_14/level_1/task_6.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Use for to print each number in the numbers list."""
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def print_numbers():
+    for number in numbers:
+        return numbers
+print(print_numbers())
